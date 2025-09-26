3b366437f82ae6b1abc0f93bac04b8b2480664bc
sjk2224 <127715484+sjk2224@users.noreply.github.com>
2025-09-26 07:50:04 +0000

    fix: Python 3.12 호환성 개선 및 테스트 통과 (distribute_setup 제거, 예외 타입 수정)

--- workshop/legacy/setup.py
+++ workshop/legacy/setup.py
@@ -1,5 +1,3 @@
-import distribute_setup
-distribute_setup.use_setuptools()
 from setuptools import setup
 
 tests_require = ['pytest']
@@ -9,9 +7,9 @@ setup(
     version = "0.0.6",
     packages = ['guachi'],
     include_package_data=True,
-    package_data = {
-        '': ['distribute_setup.py'],
-        },
+    # package_data = {
+    #     '': ['distribute_setup.py'],
+    # },
 
     # metadata
     author = "Alfredo Deza",
